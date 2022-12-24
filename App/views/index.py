74162bff910ca54908601ca02db0fb84c60a9c7b
crowbar72 <tristanleid72@gmail.com>
2022-12-24 20:26:18 +0000

    Routing from the signup form

--- App/views/index.py
+++ App/views/index.py
@@ -2,7 +2,8 @@ from flask import Blueprint, redirect, render_template, request, send_from_direc
 
 from App.controllers import (
     SignUp,
-    LogIn
+    LogIn,
+    create_user
 )
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
@@ -12,9 +13,20 @@ def index_page():
     return render_template('index.html')
 
 @index_views.route('/signuptwo', methods=['GET'])
-def signup_page():
+def signup_get_page():
     form = SignUp()
     return render_template('signup.html', form=form)
+
+@index_views.route("/signuptwo", methods=["POST"])
+def signup_post_page():
+    form = SignUp()
+    if form.validate_on_submit():
+        data = request.form
+        user = create_user(data["username"], data["password"])
+        if user:
+            flash("Account Created!")
+            return redirect('url_for("index_views.index_page")')
+    return redirect(url_for("index_views.signup_get_page"))
 
 @index_views.route('/login', methods=['GET'])
 def login_page():
